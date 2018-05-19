24d268698ba2ca03d20ff65ef7e410a0b8ee691c
Eddú Meléndez <eddu.melendez@gmail.com>
2018-05-19 10:42:00 -0500

    Initial commit

---
+++ src/main/java/io/jenkinsperu/limajam/tododemo/TodoDemoApplication.java
@@ -0,0 +1,12 @@
+package io.jenkinsperu.limajam.tododemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoDemoApplication.class, args);
+	}
+}
